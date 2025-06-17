2e0bc0d3dd009bad33bf064bce3932fb87405551
RimonChakma <rimon.chakma880@gmail.com>
2025-06-17 23:40:49 +0600

    sign up success and failed try catch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_authentication/firebase_options.dart';
 import 'package:firebase_authentication/screen/sign_up.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -29,10 +30,44 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final firebaseAuth = FirebaseAuth.instance;
+
+  String message = "";
+
+  void signUp () async {
+
+    try {
+      firebaseAuth.createUserWithEmailAndPassword(
+          email: emailController.text,
+          password: passwordController.text
+      );
+      setState(() {
+        message = "sign up success : $message";
+      });
+    }on FirebaseAuthException catch (e) {
+      setState(() {
+        message = "Login failed: ${e.message}";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(title: Text("Sign Up"),
+        leading: IconButton(onPressed: (){},
+            icon: Icon(Icons.arrow_back)
+        ),),
+      body: Padding(padding: EdgeInsets.all(15),child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(),
+          )
+      ],),),
     );
   }
 }
